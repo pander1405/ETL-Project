3ec81379852bf6761fddbd6ffea4f20edc3ce940
BrandyB <blbst36@hotmail.com>
2021-02-27 13:41:53 -0500

    Update schema and move to SQL

---
+++ SQL/CreateTables.sql
@@ -0,0 +1,24 @@
+create table author(
+author_id int primary key,
+first_name varchar(30),
+last_name varchar(30));
+
+create table book(
+book_id int primary key,
+title varchar(250))
+foreign key (author_fk) references author(author_id);
+
+create table source(
+source_id int primary key,
+source_name varchar(250),
+type varchar(30));
+
+
+create table review(
+review_id int primary key,
+book_fk int,
+source_fk int,
+rating int,
+date varchar(30),
+foreign key (source_fk) references source(source_id),
+foreign key (book_fk) references book(book_id));
