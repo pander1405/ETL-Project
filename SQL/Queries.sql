0ed45de937ed277479a033a2ad19288bd9786d36
blbst36 <42192670+blbst36@users.noreply.github.com>
2021-03-02 20:24:25 -0500

    Update Queries.sql
    Added book query

--- SQL/Queries.sql
+++ SQL/Queries.sql
@@ -2,11 +2,15 @@ select title, first_name, last_name, source_name, type, rating
 from book b
 join author a on b.author_fk = a.author_id
 join review r on b.book_id = r.book_fk
-join source s on r.source_fk = source_id
+join source s on r.source_fk = source_id;
 
 select first_name, last_name, count(review_id) as "Review_Total"
 from author a
 join book b on b.author_fk = a.author_id
 join review r on b.book_id = r.book_fk
 group by last_name, first_name
-order by "Review_Total" desc
+order by "Review_Total" desc;
+
+select title, first_name, last_name
+from book b
+inner join author a on b.author_fk = a.author_id;;
